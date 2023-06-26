935d44548f6511759c27f9a283fbe7196bf63fa3
Aleisder <tsarenko_d@inbox.ru>
2023-06-26 18:08:10 +0300

    Add problem 28

---
+++ easy/28._Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int base = 0;
+        int delta = 0;
+        while (base + delta < haystack.length()) {
+            if (haystack.charAt(base + delta) == needle.charAt(delta)) {
+                if (delta + 1 == needle.length()) {
+                    return base;
+                }
+                delta++;
+            } else {
+                base++;
+                delta = 0;
+            }
+        }
+        return -1;
+    }
+}
